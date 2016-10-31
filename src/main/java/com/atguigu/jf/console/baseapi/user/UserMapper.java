73a0184a13b2ae937d360bc3aeade3db941e89b5
ijonay <ijonay@qq.com>
2016-10-31 23:06:52 +0800

    Update UserMapper.java
    老司机带带我！

--- src/main/java/com/atguigu/jf/console/baseapi/user/UserMapper.java
+++ src/main/java/com/atguigu/jf/console/baseapi/user/UserMapper.java
@@ -7,6 +7,6 @@ import com.atguigu.jf.console.user.bean.pojo.User;
 
 public interface UserMapper {
 	public List<User> selectUserList(Map<String,Object> map);
-	
+	//这是我加的代码！
 	public Integer selectUserListCount();
 }
